1459f32e5d0d295f0661966f8869fe0d955fb51b
Shaun Ellis <shaun@sdellis.com>
2015-09-01 13:53:30 -0400

    added all teh drupal

---
+++ sites/all/themes/pul_base/template.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * @file
+ * Template overrides as well as (pre-)process and alter hooks for the
+ * pul_base theme.
+ */
+
+
+function pul_base_date_nav_title($params) {
+    $granularity = $params['granularity'];
+    $view = $params['view'];
+    $date_info = $view->date_info;
+    $link = !empty($params['link']) ? $params['link'] : FALSE;
+    $format = !empty($params['format']) ? $params['format'] : NULL;
+    
+    switch ($granularity) {
+        case 'year':
+        $title = $date_info->year;
+        $date_arg = $date_info->year;
+        break;
+
+        case 'month':
+        $format = !empty($format) ? $format : (empty($date_info->mini) ? 'F Y' : 'F Y');
+        $title = date_format_date($date_info->min_date, 'custom', $format);
+        $date_arg = $date_info->year .'-'. date_pad($date_info->month);
+        break;
+
+        case 'day':
+        $format = !empty($format) ? $format : (empty($date_info->mini) ? 'l, F j Y' : 'l, F j');
+        $title = date_format_date($date_info->min_date, 'custom', $format);
+        $date_arg = $date_info->year .'-'. date_pad($date_info->month) .'-'. date_pad($date_info->day);
+        break;
+        
+        case 'week':
+        $format = !empty($format) ? $format : (empty($date_info->mini) ? 'F j Y' : 'F j');
+        $title = t('Week of @date', array('@date' => date_format_date($date_info->min_date, 'custom', $format)));
+        $date_arg = $date_info->year .'-W'. date_pad($date_info->week);
+        break;
+    }
+    if (!empty($date_info->mini) || $link) {
+        // Month navigation titles are used as links in the mini view.
+        $attributes = array('title' => t('View full page month'));
+        $url = date_pager_url($view, $granularity, $date_arg, TRUE);
+        return l($title, $url, array('attributes' => $attributes));
+    }
+    else {
+        return $title;
+    }
+}
+
+function pul_base_form_alter(&$form, &$form_state, $form_id) {
+    switch($form_id) {
+        case 'allsearch_block_form':
+           $form['allsearch_block_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="allsearch-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide">Search</span></span>',
+            );
+            break;
+
+        case 'allsearch_form_form':
+            $form['allsearch_form_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="search-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide-sm strong">Search</span></span>',
+            );
+            break;
+        case 'catalog_block_form':
+            $form['catalog_block_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="catalog-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide-sm">Search</span></span>',
+            );
+            break;
+        case 'summon_block_form':
+            $form['summon_block_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="summon-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide-sm">Search</span></span>',
+            );
+            break;
+        case 'dbsearch_block_form':
+            $form['dbsearch_block_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="dbsearch-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide-sm">Search</span></span>',
+            );
+            break;
+        case 'music_audio_block_form':
+            $form['music_audio_block_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="audio-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide">Search</span></span>',
+            );
+            break;
+        case 'music_score_block_form':
+            $form['music_score_block_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="score-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide">Search</span></span>',
+            );
+            break;
+        case 'music_video_block_form':
+            $form['music_video_block_form']['#attributes']['placeholder'] = t('Search');
+            $form['button'] = array(
+                '#prefix' => '<button type="submit" id="video-submit-btn" name="op" class="form-submit">',
+                '#suffix' => '</button>',
+                '#markup' => '<span class="icon-search"> <span class="hide">Search</span></span>',
+            );
+            break;
+    }
+
+}
