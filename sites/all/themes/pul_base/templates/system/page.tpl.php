d0c0b271ec68271c1fddddec01f7c87d4fa77d63
Shaun Ellis <shaun@sdellis.com>
2015-09-22 15:22:40 -0400

    adding friends branding to site

--- sites/all/themes/pul_base/templates/system/page.tpl.php
+++ sites/all/themes/pul_base/templates/system/page.tpl.php
@@ -80,8 +80,12 @@
         <?php endif; ?>
         <?php print render($page['branding']); ?>
       </section>
+      <section class="friends-branding">
+        <h2><a href="<?php print $front_page; ?>" title="<?php print t('Friends of the Princeton University Library - Home'); ?>"  class="friends-logo"><img src="/sites/all/themes/pul_base/svg_icons/compressed/friends.svg" alt="<?php print t('Home'); ?>" /><span class="site-name"><?php print $site_name; ?></span></a></h2>
+        <?php print render($page['friends-branding']); ?>
+      </section>
     </div>
-    
+
     <?php print render($page['header']); ?>
     <div class="wrapper wrapper--navigation">
       <?php print render($page['navigation']); ?>
